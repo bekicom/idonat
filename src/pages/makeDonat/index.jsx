83d4b5c64b749643c46b3b1f814e6d097372e59f
jasurbekfrontend <jasurmominjonov2818@gmail.com>
2024-08-13 19:27:23 +0500

    mask

--- src/pages/makeDonat/index.jsx
+++ src/pages/makeDonat/index.jsx
@@ -6,6 +6,35 @@ const MakeDonat = () => {
   const userData = JSON.parse(localStorage.getItem("user"));
   const [anonim, setAnonim] = useState(false);
   const [komissiya, setKomissiya] = useState(true);
+  const [formattedCardNumber, setFormattedCardNumber] = useState("");
+  const [rawCardNumber, setRawCardNumber] = useState("");
+  const [formattedExpiryDate, setFormattedExpiryDate] = useState("");
+  const [rawExpiryDate, setRawExpiryDate] = useState("");
+  const handleInputChange = (event) => {
+    let value = event.target.value.replace(/\D/g, ""); // faqat raqamlar
+    if (value.length > 16) {
+      value = value.slice(0, 16); // Maksimal 16 raqam
+    }
+
+    // Masklash: 1234 5678 9012 3456
+    const formattedNumber = value.replace(/(\d{4})(?=\d)/g, "$1 ");
+
+    setFormattedCardNumber(formattedNumber);
+    setRawCardNumber(value); // Bo'shliqlarsiz karta raqami
+  };
+
+  const handleExpireInputChange = (event) => {
+    let value = event.target.value.replace(/\D/g, ""); // faqat raqamlar
+    if (value.length > 4) {
+      value = value.slice(0, 4); // Maksimal 4 raqam
+    }
+
+    // Masklash: MM/YY
+    const formattedDate = value.replace(/(\d{2})(?=\d)/g, "$1/");
+
+    setFormattedExpiryDate(formattedDate);
+    setRawExpiryDate(value); // Bo'shliqlarsiz MMYY formatidagi qiymat
+  };
   return (
     <div className="make_donat">
       <div className="donat-head">
@@ -72,9 +101,18 @@ const MakeDonat = () => {
           <p>Streamer oladi: 0 UZS</p>
           <label className="label">Karta raqami</label>
 
-          <input placeholder="0000 0000 0000 0000" type="text" />
+          <input
+            onChange={handleInputChange}
+            maxLength="19"
+            value={formattedCardNumber}
+            placeholder="0000 0000 0000 0000"
+            type="text"
+          />
           <label className="label">Yaroqlilik muddati</label>
           <input
+            onChange={handleExpireInputChange}
+            maxLength="5"
+            value={formattedExpiryDate}
             placeholder="MM/YY"
             style={{ maxWidth: "100px" }}
             type="text"
